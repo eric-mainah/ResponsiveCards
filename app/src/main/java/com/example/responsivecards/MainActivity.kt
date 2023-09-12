c6b166edd93d87b0913dee618edc74dd5f559948
eric-mainah <ericmainah01@gmail.com>
2023-09-12 15:20:25 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/responsivecards/MainActivity.kt
@@ -0,0 +1,182 @@
+package com.example.responsivecards
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.responsivecards.ui.theme.ResponsiveCardsTheme
+import androidx.compose.material3.IconButton
+import androidx.compose.ui.text.font.FontWeight
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ResponsiveCardsTheme {
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier, color = MaterialTheme.colorScheme.background) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text( "Certified Lover Boy" )
+        Button(
+            modifier = Modifier.padding(vertical = 20.dp),
+            onClick = onContinueClicked
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+@Composable
+private fun Greetings (
+    modifier: Modifier =  Modifier,
+    names: List<String> = List(25) {" $it "}
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 5.dp)){
+        items(items = names){
+            name -> Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Card (
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier.padding(vertical = 5.dp, horizontal = 10.dp)
+    ){
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var enlarge by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(15.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column (
+            modifier = Modifier
+                .weight(1f)
+                .padding(15.dp)
+        ){
+            Text(text = "Champagne Papi, ")
+            Text(
+                text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (enlarge) {
+                Text(
+                    text = ("21 my addy, so the knife is on the gate\n" +
+                            "All my dawgs eating off a Baccarat plate\n" +
+                            "People See Drake and they underestimate\n" +
+                            "Take it from a vet, that's a rookie mistake, ayy")
+                )
+            }
+        }
+        IconButton(onClick = { enlarge = !enlarge }) {
+            Icon(
+                imageVector = if (enlarge) Filled.KeyboardArrowUp else Filled.ArrowDropDown,
+                        contentDescription = if (enlarge) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 400,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Preview(showBackground = true, widthDp = 400)
+@Composable
+fun DefaultPreview() {
+    ResponsiveCardsTheme{
+        Greetings()
+    }
+}
+
+@Preview (
+    showBackground = true,
+    widthDp = 400,
+    heightDp = 400
+)
+@Composable
+fun OnboardingPreview() {
+    ResponsiveCardsTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
---
+++ app/src/main/java/com/example/responsivecards/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.responsivecards.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkOrange = Color(0xFFFF8C00)
+val DarkGreen = Color(0xFF006400)
+val LightSalmon = Color(0xFFFFA07A)
+val Chartreuse = Color(0xFFEFF7CF)
+
